fedaf988bfc1ae43c577ef24aee759a39b006fa9
janakiramkusu <160325078+janakiramkusu@users.noreply.github.com>
2025-01-09 17:07:44 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,15 @@ require("dotenv").config(); // Load environment variables
 const { app } = require("./app");
 const mongoose = require("mongoose");
 
-const dbname = process.env.DB_NAME || "satdb";
+// Load environment variables
+const dbUri = process.env.MONGO_URI; // MongoDB Atlas URI
 const port = process.env.PORT || 7001;
 
 // Connect to MongoDB
 mongoose
-  .connect(`mongodb://127.0.0.1:27017/${dbname}`)
-  .then(() => console.log(`DB is connected on: ${dbname}`))
-  .catch((err) => console.error(`DB connection error: ${err.message}`));
+  .connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.error(`MongoDB connection error: ${err.message}`));
 
 // Start the server
 app.listen(port, () => console.log(`Server is active on port: ${port}`));
